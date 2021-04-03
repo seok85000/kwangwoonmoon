da9424b6dc582f3434df1f149e40620832a13ffd
sammy310 <kipper098@gmail.com>
2021-04-04 02:18:59 +0900

    이벤트, 종목 클래스 추가

---
+++ Event.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace kwangwoonmoon
+{
+    class Event
+    {
+        // 제목
+        public string EventTitle { get; private set; }
+
+        // 정보
+        public string EventDescription { get; private set; }
+
+        // 영향을 주는 종목
+        List<Stock> influenceStock = null;
+
+        // 영향을 주는 이벤트
+        List<Event> influenceEvent = null;
+
+        // 영향을 주는 정도
+        public float InfluencePower { get; private set; }
+    }
+}
---
+++ Stock.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace kwangwoonmoon
+{
+    class Stock
+    {
+        // 종목 이름
+        public string StockName { get; private set; }
+
+        // 주가
+        public float StockPrice { get; private set; }
+
+        // 지난 턴 주가
+        float LastStockPrice { get; set; }
+
+        // 등락률
+        public float GetStockRange()
+        {
+            return StockPrice / LastStockPrice;
+        }
+    }
+}
