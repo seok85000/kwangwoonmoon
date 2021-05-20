21cd54780800021f1984b8d031cb510a7159c4b2
sammy310 <kipper098@gmail.com>
2021-05-21 00:15:06 +0900

    뉴스, 정보 폼 추가

---
+++ EventNInfo.Designer.cs
@@ -0,0 +1,119 @@
+﻿
+namespace kwangwoonmoon
+{
+    partial class EventNInfo
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.eventListView = new System.Windows.Forms.ListView();
+            this.infoListView = new System.Windows.Forms.ListView();
+            this.OKButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("굴림", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label1.Location = new System.Drawing.Point(180, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(40, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "뉴스";
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("굴림", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label2.Location = new System.Drawing.Point(565, 20);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(40, 16);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "정보";
+            // 
+            // eventListView
+            // 
+            this.eventListView.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.eventListView.HideSelection = false;
+            this.eventListView.Location = new System.Drawing.Point(30, 50);
+            this.eventListView.Name = "eventListView";
+            this.eventListView.Size = new System.Drawing.Size(340, 530);
+            this.eventListView.TabIndex = 1;
+            this.eventListView.UseCompatibleStateImageBehavior = false;
+            // 
+            // infoListView
+            // 
+            this.infoListView.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.infoListView.HideSelection = false;
+            this.infoListView.Location = new System.Drawing.Point(415, 50);
+            this.infoListView.Name = "infoListView";
+            this.infoListView.Size = new System.Drawing.Size(340, 530);
+            this.infoListView.TabIndex = 1;
+            this.infoListView.UseCompatibleStateImageBehavior = false;
+            // 
+            // OKButton
+            // 
+            this.OKButton.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.OKButton.Location = new System.Drawing.Point(340, 600);
+            this.OKButton.Name = "OKButton";
+            this.OKButton.Size = new System.Drawing.Size(100, 35);
+            this.OKButton.TabIndex = 2;
+            this.OKButton.Text = "확인";
+            this.OKButton.UseVisualStyleBackColor = true;
+            // 
+            // EventNInfo
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 661);
+            this.Controls.Add(this.OKButton);
+            this.Controls.Add(this.infoListView);
+            this.Controls.Add(this.eventListView);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.MaximumSize = new System.Drawing.Size(800, 700);
+            this.MinimumSize = new System.Drawing.Size(800, 700);
+            this.Name = "EventNInfo";
+            this.Text = "뉴스 / 정보";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ListView eventListView;
+        private System.Windows.Forms.ListView infoListView;
+        private System.Windows.Forms.Button OKButton;
+    }
+}
